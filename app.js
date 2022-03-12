f1f9213b909905b405e5b094a004e982fd1df321
husnuljahneer <husnuljahneer19@gmail.com>
2022-03-12 14:23:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express()
+const port = 3000
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+
+app.listen(port, () => {
+    console.log('Server started at port ' + port)
+})
